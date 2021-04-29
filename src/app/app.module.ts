481b620c96be184633c4b5ce9ffde94f06965627
Vlad Sevostianov <flypsoff@gmail.com>
2021-04-29 21:01:50 +0300

    added to: declarations two components,  imports FormsModule (for two ways binding [(ngModel)]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
+import { MaterialModule } from './material/material.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { MaterialModule } from './material/material.module';
+import { NopeComponent } from './nope/nope.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +19,15 @@ import { MaterialModule } from './material/material.module';
     HomeComponent,
     AboutComponent,
     NotFoundComponent,
+    NopeComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
