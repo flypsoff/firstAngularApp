93e000b8e456eceff2e3d2d70f83c3804ec9207e
Vlad Sevostianov <flypsoff@gmail.com>
2021-04-29 20:53:51 +0300

    added some link for showing redirect and 404 page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, OnDestroy {
   constructor() {}
 
   ngOnInit(): void {
     console.log('ABOUT page was mount');
+  }
+  ngOnDestroy(): void {
+    console.log('ABOUT page was destroyed');
   }
 }
