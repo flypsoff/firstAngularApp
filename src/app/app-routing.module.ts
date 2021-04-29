2a77e07db97bde4995ea310af55330d1b6984d44
Vlad Sevostianov <flypsoff@gmail.com>
2021-04-29 20:58:06 +0300

    added two path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { NopeComponent } from './nope/nope.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'nope', component: NopeComponent },
+  { path: 'redirect', redirectTo: 'nope' },
   { path: '**', component: NotFoundComponent },
 ];
 
