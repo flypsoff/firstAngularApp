018106d3e4ba89e54ebc246abe1d16c3de7e11c3
Vlad Sevostianov <flypsoff@gmail.com>
2021-04-29 20:56:44 +0300

    here we import and export all class from @angular/material that we use in our app

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
-  imports: [MatToolbarModule],
-  exports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatInputModule, MatButtonModule, MatBadgeModule],
+  exports: [MatToolbarModule, MatInputModule, MatButtonModule, MatBadgeModule],
 })
 export class MaterialModule {}
