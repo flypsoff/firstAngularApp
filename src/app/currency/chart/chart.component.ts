45b2592267a470a1554c78204254fd02b3d940e8
Vlad Sevostianov <flypsoff@gmail.com>
2021-05-11 10:52:29 +0300

    component for showing chart

---
+++ src/app/currency/chart/chart.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+import { NBUI } from '../currency.component';
+
+@Component({
+  selector: 'app-chart',
+  templateUrl: './chart.component.html',
+  styleUrls: ['./chart.component.scss'],
+})
+export class ChartComponent {
+  title = 'dashboard';
+  chart: any;
+  constructor() {}
+
+  showChart(currencies: Array<NBUI>, type: string): void {
+    Chart.register(...registerables);
+    this.chart = new Chart('canvas', {
+      type: 'line',
+      data: {
+        labels: currencies.map((item: NBUI) => item.exchangedate),
+        datasets: [
+          {
+            label: type || 'USD',
+            data: currencies.map((item: NBUI) => item.rate),
+            backgroundColor: 'blue',
+            borderColor: 'blue',
+            fill: false,
+          },
+        ],
+      },
+    });
+  }
+}
