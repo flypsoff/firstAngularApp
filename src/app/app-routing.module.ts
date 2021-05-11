07584e252aeb64f9a19aec35f8c650dabbe449fe
Vlad Sevostianov <flypsoff@gmail.com>
2021-05-11 11:01:08 +0300

    settings for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AboutComponent } from './about/about.component';
+import { CurrencyComponent } from './currency/currency.component';
 import { HomeComponent } from './home/home.component';
+import { ItemsDetailsComponent } from './items/items-details/items-details.component';
+import { ItemsInfoComponent } from './items/items-info/items-info.component';
+import { ItemsComponent } from './items/items.component';
 import { NopeComponent } from './nope/nope.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
@@ -10,6 +14,18 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'nope', component: NopeComponent },
+  { path: 'currency', component: CurrencyComponent },
+  {
+    path: 'items/:id',
+    component: ItemsComponent,
+    children: [
+      {
+        path: 'info',
+        component: ItemsInfoComponent,
+      },
+      { path: 'details', component: ItemsDetailsComponent },
+    ],
+  },
   { path: 'redirect', redirectTo: 'nope' },
   { path: '**', component: NotFoundComponent },
 ];
