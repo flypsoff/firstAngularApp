1d7b7004ca8a8e2dddeaf9e75c714583e5d63ba8
Vlad Sevostianov <flypsoff@gmail.com>
2021-05-11 11:04:27 +0300

    module only for import, export angular material modules

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,9 +3,31 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-  imports: [MatToolbarModule, MatInputModule, MatButtonModule, MatBadgeModule],
-  exports: [MatToolbarModule, MatInputModule, MatButtonModule, MatBadgeModule],
+  imports: [
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatIconModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
+  exports: [
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatIconModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
 })
 export class MaterialModule {}
